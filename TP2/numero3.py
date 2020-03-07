a4c74d47d33ddb267645d16a1c7694a6ff1d30f3
Theonegvb <59982613+Theonegvb@users.noreply.github.com>
2020-03-07 08:52:31 -0500

    Création de fonction à intégré et celle de changement de variable
    J'ai créé des fonctions qui permette de faire le spectre et le changement de variable des bornes.

--- TP2/numero3.py
+++ TP2/numero3.py
@@ -1,5 +1,5 @@
 from gaussxw import gaussxw
-
+import numpy as np
 
 def integrale_gausienne(fonction, a, b, N):
     """
@@ -22,3 +22,13 @@ def integrale_gausienne(fonction, a, b, N):
 
     return s
 
+def fonction_spectre_electromag(x):
+    return x ** 3 / (np.exp(x) - 1)
+
+def changement_variable(lamb, T):
+
+    h = 6.62067004e-34
+    c = 299792458
+    kB = 1.38064852e-23
+    
+    return h * c / (lamb * kB * T)
