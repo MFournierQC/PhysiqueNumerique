34db79cc5629632a640440e428bf13665d7e3c9e
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-03-21 13:39:12 -0400

    Num 1 d

--- TP3/AnharmonicOscillator.py
+++ TP3/AnharmonicOscillator.py
@@ -18,13 +18,15 @@ class AnharmonicOscillator:
         h = (self.end - self.start) / n
         tpoints = np.arange(self.start, self.end, h)
         xValues = []
+        sValues = []
         x = np.array([x1, x2], float)
 
         for t in tpoints:
             xValues.append(x[0])
+            sValues.append(x[1])
             k1 = h * self.coupledEquation(x, t)
             k2 = h * self.coupledEquation(x + 0.5 * k1, t)
             k3 = h * self.coupledEquation(x + 0.5 * k2, t)
             k4 = h * self.coupledEquation(x + k3, t)
             x += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        return tpoints, xValues
+        return tpoints, xValues, sValues
--- TP3/numero1.py
+++ TP3/numero1.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     oscillator = HarmonicOscillator(1, 0, 50)
 
     # Sous question a :
-    tpoints, xValues = oscillator.methodRK4(1000, 1.0, 0.0)
+    tpoints, xValues, sValues = oscillator.methodRK4(1000, 1.0, 0.0)
     plt.subplot(2, 1, 1)
     plt.plot(tpoints, xValues)
     plt.ylabel('ODE')
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     plt.show()
 
     # Sous question b :
-    x1 = [0.0, 0.5, 1.0, 1.5, 2.0]
-    for x in x1:
-        label = 'x(0)={}'.replace('{}', str(x))
-        tpoints, xValues = oscillator.methodRK4(1000, x, 0.0)
+    origins = [0.0, 0.5, 1.0, 1.5, 2.0]
+    for origin in origins:
+        label = 'x(0)={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues= oscillator.methodRK4(1000, origin, 0.0)
         plt.plot(tpoints, xValues, label=label)
     plt.ylabel('ODE')
     plt.xlabel('t')
@@ -32,12 +32,50 @@ if __name__ == '__main__':
 
     # Sous question c :
     aOscillator = AnharmonicOscillator(1, 0, 50)
-    x1 = [0.0, 1.0, 2.0]
-    for x in x1:
-        label = 'x(0)={}'.replace('{}', str(x))
-        tpoints, xValues = aOscillator.methodRK4(1000, x, 0.0)
+    origins = [0.0, 1.0, 2.0]
+    for origin in origins:
+        label = 'x(0)={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues = aOscillator.methodRK4(1000, origin, 0.0)
         plt.plot(tpoints, xValues, label=label)
     plt.ylabel('ODE')
     plt.xlabel('t')
+    plt.legend()
+    plt.show()
+
+    # Sous question d :
+    origins = [0.0, 0.5, 1.0, 1.5, 2.0]
+    plt.subplot(2, 1, 1)
+    for origin in origins:
+        label = 'x(0)={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues = oscillator.methodRK4(1000, origin, 0.0)
+        plt.plot(xValues, sValues, label=label)
+    plt.ylabel('Harmonic ODE')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    for origin in origins:
+        label = 'x(0)={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues = aOscillator.methodRK4(1000, origin, 0.0)
+        plt.plot(xValues, sValues, label=label)
+    plt.ylabel('Anharmonic ODE')
+    plt.xlabel('x')
+    plt.legend()
+    plt.show()
+
+    plt.subplot(2, 1, 1)
+    for origin in origins:
+        label = r'$\dot{x}(0)$={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues = oscillator.methodRK4(1000, 1.0, origin)
+        plt.plot(xValues, sValues, label=label)
+    plt.ylabel('Harmonic ODE')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    for origin in origins:
+        label = r'$\dot{x}(0)$={}'.replace('{}', str(origin))
+        tpoints, xValues, sValues = aOscillator.methodRK4(1000, 1.0, origin)
+        plt.plot(xValues, sValues, label=label)
+    plt.ylabel('Anharmonic ODE')
+    plt.xlabel('x')
     plt.legend()
     plt.show()
