a07569547202183a233512af42d8c8527d315041
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-03-21 12:54:06 -0400

    Num 1 c

---
+++ TP3/AnharmonicOscillator.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class AnharmonicOscillator:
+    def __init__(self, omega, start, end):
+        self.omega = omega
+        self.start = start
+        self.end = end
+
+    def coupledEquation(self, x, t=0):
+        x1 = x[0]
+        x2 = x[1]
+        fx1 = x2
+        fx2 = -(self.omega ** 2) * (x1 ** 3)
+        return np.array([fx1, fx2], float)
+
+    def methodRK4(self, n, x1=0, x2=0, t=0):
+        h = (self.end - self.start) / n
+        tpoints = np.arange(self.start, self.end, h)
+        xValues = []
+        x = np.array([x1, x2], float)
+
+        for t in tpoints:
+            xValues.append(x[0])
+            k1 = h * self.coupledEquation(x, t)
+            k2 = h * self.coupledEquation(x + 0.5 * k1, t)
+            k3 = h * self.coupledEquation(x + 0.5 * k2, t)
+            k4 = h * self.coupledEquation(x + k3, t)
+            x += (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        return tpoints, xValues
--- TP3/numero1.py
+++ TP3/numero1.py
@@ -1,4 +1,5 @@
 from TP3.HarmonicOscillator import HarmonicOscillator
+from TP3.AnharmonicOscillator import AnharmonicOscillator
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -23,6 +24,18 @@ if __name__ == '__main__':
     for x in x1:
         label = 'x(0)={}'.replace('{}', str(x))
         tpoints, xValues = oscillator.methodRK4(1000, x, 0.0)
+        plt.plot(tpoints, xValues, label=label)
+    plt.ylabel('ODE')
+    plt.xlabel('t')
+    plt.legend()
+    plt.show()
+
+    # Sous question c :
+    aOscillator = AnharmonicOscillator(1, 0, 50)
+    x1 = [0.0, 1.0, 2.0]
+    for x in x1:
+        label = 'x(0)={}'.replace('{}', str(x))
+        tpoints, xValues = aOscillator.methodRK4(1000, x, 0.0)
         plt.plot(tpoints, xValues, label=label)
     plt.ylabel('ODE')
     plt.xlabel('t')
